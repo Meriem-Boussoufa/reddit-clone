31b971151cb0a5a17bb14a34833bb6b9a965ad6d
meriem <meriem@gmail.com>
2024-03-02 20:16:08 +0100

    storing data to provider

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -3,17 +3,31 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone/features/auth/repository/auth_repository.dart';
 
 import '../../../core/utils.dart';
+import '../../../models/user_model.dart';
 
-final authControllerProvider = Provider(
-    (ref) => AuthController(authRepository: ref.read(authRepositoryProvider)));
+final userProvider = StateProvider<UserModel?>((ref) => null);
 
-class AuthController {
+final authControllerProvider =
+    StateNotifierProvider<AuthController, bool>((ref) => AuthController(
+          authRepository: ref.watch(authRepositoryProvider),
+          ref: ref,
+        ));
+
+class AuthController extends StateNotifier<bool> {
   final AuthRepository _authRepository;
-  AuthController({required AuthRepository authRepository})
-      : _authRepository = authRepository;
+  final Ref _ref;
+  AuthController({required AuthRepository authRepository, required Ref ref})
+      : _authRepository = authRepository,
+        _ref = ref,
+        super(false); // Loading
 
   void signInWithGoogle(BuildContext context) async {
+    state = true;
     final user = await _authRepository.signInWithGoogle();
-    user.fold((error) => showSnackBar(context, error), (r) => null);
+    state = false;
+    user.fold(
+        (error) => showSnackBar(context, error),
+        (userModel) =>
+            _ref.read(userProvider.notifier).update((state) => userModel));
   }
 }
--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/core/common/loader.dart';
 import 'package:reddit_clone/core/common/sign_in_button.dart';
 import 'package:reddit_clone/core/constants/constants.dart';
 
-class LoginScreen extends StatefulWidget {
+import '../controller/auth_controller.dart';
+
+class LoginScreen extends ConsumerWidget {
   const LoginScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isLoading = ref.watch(authControllerProvider);
     return Scaffold(
       appBar: AppBar(
         title: Image.asset(
@@ -27,27 +27,29 @@ class _LoginScreenState extends State<LoginScreen> {
               ))
         ],
       ),
-      body: Column(children: [
-        const SizedBox(height: 30),
-        const Text(
-          'Dive into anything',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 0.5,
-          ),
-        ),
-        const SizedBox(height: 30),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Image.asset(
-            Constants.loginEmotePath,
-            height: 400,
-          ),
-        ),
-        const SizedBox(height: 20),
-        const SignInButton(),
-      ]),
+      body: isLoading
+          ? const Loader()
+          : Column(children: [
+              const SizedBox(height: 30),
+              const Text(
+                'Dive into anything',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 0.5,
+                ),
+              ),
+              const SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset(
+                  Constants.loginEmotePath,
+                  height: 400,
+                ),
+              ),
+              const SizedBox(height: 20),
+              const SignInButton(),
+            ]),
     );
   }
 }
