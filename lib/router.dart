50eee28da9599fa05ab9f4762096b37ac576cff0
meriem <meriem@gmail.com>
2024-03-03 19:43:07 +0100

    add mod tools ui

--- lib/router.dart
+++ lib/router.dart
@@ -4,6 +4,8 @@ import 'package:routemaster/routemaster.dart';
 
 import 'features/community/screens/community_screen.dart';
 import 'features/community/screens/create_community_screen.dart';
+import 'features/community/screens/edit_community_screen.dart';
+import 'features/community/screens/mod_tools_screen.dart';
 import 'features/home_screen.dart';
 import 'features/auth/screens/login_screen.dart';
 
@@ -19,6 +21,16 @@ final loggedInRoute = RouteMap(
     '/r/:name': (route) => MaterialPage(
           child: CommunityScreen(
             name: route.pathParameters['name']!,
+          ),
+        ),
+    '/mod-tools/:name': (routeData) => MaterialPage(
+          child: ModToolsScreen(
+            name: routeData.pathParameters['name']!,
+          ),
+        ),
+    '/edit-community/:name': (routeData) => MaterialPage(
+          child: EditCommunityScreen(
+            name: routeData.pathParameters['name']!,
           ),
         ),
   },
