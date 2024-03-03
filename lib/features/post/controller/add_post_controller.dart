f5a46872bf435cdcfbc58bdecb40cf9423d2948a
meriem <meriem@gmail.com>
2024-03-03 22:11:18 +0100

    sharing link image and text posts

---
+++ lib/features/post/controller/add_post_controller.dart
@@ -0,0 +1,145 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:routemaster/routemaster.dart';
+import '../../../core/providers/storage_repository_provider.dart';
+import '../../../core/utils.dart';
+import '../../../models/community.dart';
+import '../../../models/post.dart';
+import '../../auth/controller/auth_controller.dart';
+import 'package:uuid/uuid.dart';
+import '../repository/add_post_repository.dart';
+
+final postControllerProvider =
+    StateNotifierProvider<PostController, bool>((ref) {
+  final postRepository = ref.watch(postRepositoryProvider);
+  final storageRepository = ref.watch(storageRepositoryProvider);
+  return PostController(
+    postRepository: postRepository,
+    storageRepository: storageRepository,
+    ref: ref,
+  );
+});
+
+class PostController extends StateNotifier<bool> {
+  final PostRepository _postRepository;
+  final Ref _ref;
+  final StorageRepository _storageRepository;
+  PostController({
+    required PostRepository postRepository,
+    required Ref ref,
+    required StorageRepository storageRepository,
+  })  : _postRepository = postRepository,
+        _ref = ref,
+        _storageRepository = storageRepository,
+        super(false);
+  void shareTextPost({
+    required BuildContext context,
+    required String title,
+    required Community selectedCommunity,
+    required String description,
+  }) async {
+    state = true;
+    String postId = const Uuid().v1();
+    final user = _ref.read(userProvider)!;
+    final Post post = Post(
+      id: postId,
+      title: title,
+      communityName: selectedCommunity.name,
+      communityProfilePic: selectedCommunity.avatar,
+      upvotes: [],
+      downvotes: [],
+      commentCount: 0,
+      username: user.name,
+      uid: user.uid,
+      type: 'text',
+      createdAt: DateTime.now(),
+      awards: [],
+      description: description,
+    );
+    final res = await _postRepository.addPost(post);
+    state = false;
+    res.fold((l) => showSnackBar(context, l.message), (r) {
+      showSnackBar(context, 'Posted successfully!');
+      Routemaster.of(context).pop();
+    });
+  }
+
+  void shareLinkPost({
+    required BuildContext context,
+    required String title,
+    required Community selectedCommunity,
+    required String link,
+  }) async {
+    state = true;
+    String postId = const Uuid().v1();
+    final user = _ref.read(userProvider)!;
+
+    final Post post = Post(
+      id: postId,
+      title: title,
+      communityName: selectedCommunity.name,
+      communityProfilePic: selectedCommunity.avatar,
+      upvotes: [],
+      downvotes: [],
+      commentCount: 0,
+      username: user.name,
+      uid: user.uid,
+      type: 'link',
+      createdAt: DateTime.now(),
+      awards: [],
+      link: link,
+    );
+    final res = await _postRepository.addPost(post);
+    state = false;
+    res.fold((l) => showSnackBar(context, l.message), (r) {
+      showSnackBar(context, 'Posted successfully!');
+      Routemaster.of(context).pop();
+    });
+  }
+
+  void shareImagePost({
+    required BuildContext context,
+    required String title,
+    required Community selectedCommunity,
+    required File? file,
+    required Uint8List? webFile,
+  }) async {
+    state = true;
+    String postId = const Uuid().v1();
+    final user = _ref.read(userProvider)!;
+    final imageRes = await _storageRepository.storeFile(
+      path: 'posts/${selectedCommunity.name}',
+      id: postId,
+      file: file,
+      webFile: webFile,
+    );
+
+    imageRes.fold((l) => showSnackBar(context, l.message), (r) async {
+      final Post post = Post(
+        id: postId,
+        title: title,
+        communityName: selectedCommunity.name,
+        communityProfilePic: selectedCommunity.avatar,
+        upvotes: [],
+        downvotes: [],
+        commentCount: 0,
+        username: user.name,
+        uid: user.uid,
+        type: 'image',
+        createdAt: DateTime.now(),
+        awards: [],
+        link: r,
+      );
+
+      final res = await _postRepository.addPost(post);
+      state = false;
+      res.fold((l) => showSnackBar(context, l.message), (r) {
+        showSnackBar(context, 'Posted successfully!');
+        Routemaster.of(context).pop();
+      });
+    });
+  }
+}
---
+++ lib/features/post/repository/add_post_repository.dart
@@ -0,0 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpdart/fpdart.dart';
+import '../../../core/constants/firebase_constants.dart';
+import '../../../core/failure.dart';
+import '../../../core/providers/firebase_providers.dart';
+import '../../../core/type_defs.dart';
+import '../../../models/post.dart';
+
+final postRepositoryProvider = Provider((ref) {
+  return PostRepository(
+    firestore: ref.watch(firestoreProvider),
+  );
+});
+
+class PostRepository {
+  final FirebaseFirestore _firestore;
+  PostRepository({required FirebaseFirestore firestore})
+      : _firestore = firestore;
+
+  CollectionReference get _posts =>
+      _firestore.collection(FirebaseConstants.postsCollection);
+  CollectionReference get _comments =>
+      _firestore.collection(FirebaseConstants.commentsCollection);
+  CollectionReference get _users =>
+      _firestore.collection(FirebaseConstants.usersCollection);
+
+  FutureVoid addPost(Post post) async {
+    try {
+      return right(_posts.doc(post.id).set(post.toMap()));
+    } on FirebaseException catch (e) {
+      throw e.message!;
+    } catch (e) {
+      return left(Failure(e.toString()));
+    }
+  }
+
+  Stream<Post> getPostById(String postId) {
+    return _posts
+        .doc(postId)
+        .snapshots()
+        .map((event) => Post.fromMap(event.data() as Map<String, dynamic>));
+  }
+}
